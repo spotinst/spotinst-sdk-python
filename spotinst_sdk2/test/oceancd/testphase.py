b5eb344fb7fd1500d4f2018607a4f393b5ae2cfa
Amrutha Ramanathan <105342664+ramrutha497@users.noreply.github.com>
2023-05-19 13:23:18 +0530

    test file update

---
+++ spotinst_sdk2/test/oceancd/testphase.py
@@ -0,0 +1,11 @@
+from spotinst_sdk2 import SpotinstSession
+from spotinst_sdk2.models.ocean_cd import *
+
+session = SpotinstSession(auth_token='30b3dd9d0d62ebf0c0b702027f0ce2d1a2ddfb96347f6993e0f079c1cc904034',
+                          base_url='https://api.spotinst.io')
+client = session.client("oceancd", log_level="debug")
+# Create elastigroup and retrieve group id
+# response = client.ocean_cd_describe_rollout_phase(rollout_id='rol-d80f2865def6')
+# print(response)
+response = client.get_oceancd_verification_provider(name='Anurag-Experiment-Prometheus')
+print(response)
