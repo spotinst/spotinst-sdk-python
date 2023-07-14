83f0554964ec5076e26a10792fa70fc4a9034dfe
adityas1netapp <adityas1@netapp.com>
2023-07-14 15:30:33 +0530

    updated

--- spotinst_sdk2/models/admin/user_mapping.py
+++ spotinst_sdk2/models/admin/user_mapping.py
@@ -28,8 +28,6 @@ class UserMappingRequest:
     def toJSON(self):
         return json.dumps(self, default=lambda o: o.__dict__,
                           sort_keys=True, indent=4)
-    
-#endregion
 
 
 
