806cec934b50d6feea045d5162e018172e803d98
Anurag Sharma <anurag4@netapp.com>
2024-10-28 10:55:53 +0530

    User, Policy, User Group Related APIs (#188)

--- docs/models/admin.md
+++ docs/models/admin.md
@@ -16,3 +16,84 @@ __Arguments__
 - __account_id__: str
 - __role__: str
 
+<h1 id="spotinst_sdk2.models.admin.organization">spotinst_sdk2.models.admin.organization</h1>
+
+
+<h2 id="spotinst_sdk2.models.admin.organization.Statement">Statement</h2>
+
+```python
+Statement(self,
+          effect: str = 'd3043820717d74d9a17694c176d39733',
+          actions: typing.List[str] = None,
+          resources: typing.List[str] = None)
+```
+
+__Arguments__
+
+- __effect__: str
+- __actions__: List[str]
+- __resources__: List[str]
+
+<h2 id="spotinst_sdk2.models.admin.organization.PolicyContent">PolicyContent</h2>
+
+```python
+PolicyContent(
+    self,
+    statements:
+    typing.List[spotinst_sdk2.models.admin.organization.Statement] = 'd3043820717d74d9a17694c176d39733'
+)
+```
+
+__Arguments__
+
+- __statements__: List[Statement]
+
+<h2 id="spotinst_sdk2.models.admin.organization.AccessPolicy">AccessPolicy</h2>
+
+```python
+AccessPolicy(
+  self,
+  description: str = 'd3043820717d74d9a17694c176d39733',
+  name: str = 'd3043820717d74d9a17694c176d39733',
+  policy_content: PolicyContent = 'd3043820717d74d9a17694c176d39733')
+```
+
+__Arguments__
+
+- __description__: str
+- __name__: str
+- __policy_content__: PolicyContent
+
+<h2 id="spotinst_sdk2.models.admin.organization.PolicyMapping">PolicyMapping</h2>
+
+```python
+PolicyMapping(
+  self,
+  policy_id: str = 'd3043820717d74d9a17694c176d39733',
+  account_ids: typing.List[str] = 'd3043820717d74d9a17694c176d39733')
+```
+
+__Arguments__
+
+- __account_ids__: List[str]
+- __policy_id__: str
+
+<h2 id="spotinst_sdk2.models.admin.organization.UserGroup">UserGroup</h2>
+
+```python
+UserGroup(
+  self,
+  description: str = 'd3043820717d74d9a17694c176d39733',
+  name: str = 'd3043820717d74d9a17694c176d39733',
+  policies:
+    typing.List[spotinst_sdk2.models.admin.organization.PolicyMapping] = 'd3043820717d74d9a17694c176d39733',
+  user_ids: typing.List[str] = 'd3043820717d74d9a17694c176d39733')
+```
+
+__Arguments__
+
+- __description__: str
+- __name__: str
+- __policies__: List[PolicyMapping]
+- __user_ids__: List[str]
+
---
+++ spotinst_sdk2/models/admin/organization.py
@@ -0,0 +1,118 @@
+import json
+from typing import List
+
+none = "d3043820717d74d9a17694c176d39733"
+
+# region organization
+
+
+class Statement:
+    """
+    # Arguments
+    effect: str
+    actions: List[str]
+    resources: List[str]
+    """
+
+    def __init__(
+            self,
+            effect: str = none,
+            actions: List[str] = None,
+            resources: List[str] = None):
+
+        self.effect = effect
+        self.actions = actions
+        self.resources = resources
+
+
+class PolicyContent:
+    """
+    # Arguments
+    statements: List[Statement]
+    """
+
+    def __init__(
+            self,
+            statements: List[Statement] = none):
+
+        self.statements = statements
+
+
+class AccessPolicy:
+    """
+        # Arguments
+        description: str
+        name: str
+        policy_content: PolicyContent
+        """
+
+    def __init__(
+            self,
+            description: str = none,
+            name: str = none,
+            policy_content: PolicyContent = none):
+
+        self.description = description
+        self.name = name
+        self.policy_content = policy_content
+
+
+class PolicyMapping:
+    """
+    # Arguments
+    account_ids: List[str]
+    policy_id: str
+    """
+
+    def __init__(
+            self,
+            policy_id: str = none,
+            account_ids: List[str] = none):
+
+        self.policy_id = policy_id
+        self.account_ids = account_ids
+
+
+class UserGroup:
+    """
+        # Arguments
+        description: str
+        name: str
+        policies: List[PolicyMapping]
+        user_ids: List[str]
+        """
+
+    def __init__(
+            self,
+            description: str = none,
+            name: str = none,
+            policies: List[PolicyMapping] = none,
+            user_ids: List[str] = none):
+
+        self.description = description
+        self.name = name
+        self.policies = policies
+        self.user_ids = user_ids
+
+# endregion
+
+
+class AccessPolicyCreationRequest:
+    def __init__(self, policy: AccessPolicy):
+        self.policy = policy
+
+    def toJSON(self):
+        return json.dumps(self, default=lambda o: o.__dict__,
+                          sort_keys=True, indent=4)
+
+
+class UserGroupCreationRequest:
+    def __init__(self, group: UserGroup):
+        self.description = group.description
+        self.name = group.name
+        self.policies = group.policies
+        self.user_ids = group.user_ids
+
+    def toJSON(self):
+        return json.dumps(self, default=lambda o: o.__dict__,
+                          sort_keys=True, indent=4)
